ddddef1bc54bc6b9e4fcbefebd6a42387389d181
mobynick <mobynick365@gmail.com>
2020-08-27 11:04:31 +0300

    Added position menu

--- bot.py
+++ bot.py
@@ -1,7 +1,27 @@
 import telebot
+from telebot import types
 
-bot = telebot.Telebot('1266560701:AAGq4ANITk06EVip5uUrlF5g1KP2OTo5xhE')
+bot = telebot.TeleBot('1266560701:AAGq4ANITk06EVip5uUrlF5g1KP2OTo5xhE')
+
 
 @bot.message_handler(commands=['start'])
-def menu(message):
-    pass
+def send_menu(msg):
+    markup = types.ReplyKeyboardMarkup(row_width=5)
+    markup.add(
+        types.KeyboardButton("1", callback_data="position_1"),
+        types.KeyboardButton("2", callback_data="position_2"),
+        types.KeyboardButton("3", callback_data="position_3"),
+        types.KeyboardButton("4", callback_data="position_4"),
+        types.KeyboardButton("5", callback_data="position_5")
+    )
+    bot.send_message(
+        msg.chat.id, 
+        "Выберите вашу позицию (1 - кери легкой линии, 2 - мидер, 3 - хардлайнер, 4 - семисаппорт, 5 - саппорт)",
+        reply_markup=markup
+        )
+
+
+
+
+if __name__ == "__main__":
+    bot.polling()
